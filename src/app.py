5f752e8144573926f1518b686f3df165b46c24a9
Charles Suresh <emailcharlesjosh@gmail.com>
2021-01-24 09:42:57 +0530

    added dynamic func to geoplot

--- src/app.py
+++ src/app.py
@@ -15,11 +15,11 @@ sum_dict = {crime_list[i]: sum_list[i] for i in range(len(crime_list))}
 rate_dict = {crime_list[i]: rate_list[i] for i in range(len(crime_list))}
 crime_dict = {'sum_dict': sum_dict, 'rate_dict': rate_dict}
 
-data_raw = pd.read_csv("data/ucr_crime_1975_2015.csv")
+data_raw = pd.read_csv("data/raw/ucr_crime_1975_2015.csv")
 
 def data_processing(data):
     data['state'] = data['ORI'].str[:2]
-    states = pd.read_csv('./states.csv')
+    states = pd.read_csv('data/raw/states.csv')
     data_with_state = pd.merge(data, states, how = 'left', left_on = 'state', right_on = 'Abbreviation')
     data_with_state = data_with_state.drop(['state', 'Abbreviation', 'url', 'source'], axis = 1)
     return data_with_state
@@ -32,23 +32,14 @@ def data_filtering_geochart(state, crime, metric, year_range, data_crime):
     pop = data.population_engineers_hurricanes()
     if year_range is not None:
         data_crime = data_crime.loc[data_crime["year"].between(year_range[0], year_range[1])]
-    results = data_crime.groupby('State')['violent_per_100k'].sum()
-    # crimes = [crime_dict[metric][x] for x in crime]
-    # results = data_crime.groupby('State')[[crimes]].sum()
-    results.to_frame()
+    crimes = [crime_dict[metric][x] for x in crime]
+    results = (data_crime[['State'] + crimes]
+                .melt(id_vars = "State", var_name = "crime", value_name = "crime_count")
+                .groupby('State')
+                .sum())
     results_df = pd.merge(results, pop, how = 'right', left_on = 'State', right_on = 'state')
     return results_df
 
-def data_filtering_geochart(state, crime, metric, year_range, data_crime):
-    pop = data.population_engineers_hurricanes()
-    if year_range is not None:
-        data_crime = data_crime.loc[data_crime["year"].between(year_range[0], year_range[1])]
-    results = data_crime.groupby('State')['violent_per_100k'].sum()
-    # crimes = [crime_dict[metric][x] for x in crime]
-    # results = data_crime.groupby('State')[[crimes]].sum()
-    results.to_frame()
-    results_df = pd.merge(results, pop, how = 'right', left_on = 'State', right_on = 'state')
-    return results_df
 
 def data_filtering_trendchart(state, crime, metric, year_range, data_crime):
     
@@ -86,7 +77,7 @@ app.layout = dbc.Container([
         dbc.Col(
             html.Iframe(
                 id = 'geochart',
-                style = {'border-width':'0', 'width':'100%', 'height': '400px'})
+                style = {'border-width':'0', 'width':'200%', 'height': '400px'})
         )
     ]),
     dbc.Row([
@@ -115,7 +106,7 @@ app.layout = dbc.Container([
         dbc.Col([
             html.Iframe(
                 id = 'trendchart',
-                style = {'border-width':'0', 'width':'100%', 'height': '400px'})
+                style = {'border-width':'0', 'width':'70%', 'height': '400px'})
         ])
     ])
 ])
@@ -132,10 +123,10 @@ def plot_geochart(state, crime, year_range, metric):
     print('You have selected "{}"'.format(crime))
     results_df = data_filtering_geochart(state, crime, metric, year_range, data_crime)
     states = alt.topo_feature(data.us_10m.url, 'states')
-    geo_chart = alt.Chart(states).mark_geoshape().encode(color='violent_per_100k:Q'
+    geo_chart = alt.Chart(states).mark_geoshape().encode(color='crime_count:Q'
     ).transform_lookup(
     lookup='id',
-    from_=alt.LookupData(results_df, 'id', ['violent_per_100k'])
+    from_=alt.LookupData(results_df, 'id', ['crime_count'])
     ).properties(width=500, height=300
     ).project(type='albersUsa'
     )
